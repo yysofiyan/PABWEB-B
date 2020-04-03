ca0fb6f5d872dfd438d1b7a9d18754fe2815a9a1
srirahayu22 <a2.1800127@mhs.stmik-sumedang.ac.id>
2020-04-03 13:31:15 +0700

    menambahkan folder siswa

---
+++ A2.1800127/Praktikum_1/siswa/config/database.php
@@ -0,0 +1,19 @@
+ <?php               // tag pembuka syntax PHP
+ // set default timezone
+ date_default_timezone_set("ASIA/JAKARTA");
+
+ // deklarasi parameter koneksi database
+ $server   = "localhost";            // server database, default “localhost” atau “127.0.0.1”
+ $username = "root";                 // username database, default “root”
+ $password = "";                     // password database, default kosong
+ $database = "db_sekolah";           // memilih database yang akan digunakan
+
+ // koneksi database
+ $db = mysqli_connect($server, $username, $password, $database);
+
+ // cek koneksi
+ if (!$db) {
+    // cek koneksi gagal, tampilkan pesan gagal
+    die('Koneksi Database Gagal : '.mysqli_coonect_error());
+ }
+ ?>                  <!-- tag penutup syntax PHP -->        

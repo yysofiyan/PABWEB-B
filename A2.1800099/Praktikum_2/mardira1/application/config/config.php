9d7524521e07d2ded89abf2c987882eae96280a5
NurulWulan17 <a2.1800099@mhs.stmik-sumedang.ac.id>
2020-04-09 21:24:44 +0700

    mengerjkan codeigniter#4 dan mengganti nama folder

--- A2.1800099/Praktikum_2/mardira1/application/config/config.php
+++ A2.1800099/Praktikum_2/mardira1/application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
-
+$config['base_url'] = 'http://localhost/mardira1';
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -35,7 +34,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------

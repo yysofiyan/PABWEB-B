5ea5e249a1dc127edb85a073aa114f4df65990cb
danniramdhanisamsudin <a2.1800034@mhs.stmik-sumedang.ac.id>
2020-04-10 15:25:00 +0700

    Mengerjakan Modul-3 CodeIgniter

---
+++ A2.1800034/Praktikum_2/CI/modul_3/neira/application/views/mahasiswa_v.php
@@ -0,0 +1,24 @@
+<h3>Data Mahasiswa</h3>
+<table border="2">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Program Studi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $no = 1;
+        foreach ($mahasiswa as $row) { ?>
+            <tr>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $row->nim ?></td>
+                <td><?php echo $row->nama_mhs ?></td>
+                <td><?php echo $row->nama_prodi ?></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+<?php echo anchor('home', 'kembali ke home') ?>

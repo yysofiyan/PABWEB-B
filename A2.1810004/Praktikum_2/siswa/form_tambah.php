535e21427115eebffbdbd511a7433e5421fc59ef
Fadhliatul Akbar <a2.1810004@mhs.stmik-sumedang.ac.id>
2020-04-12 17:22:32 +0700

    revisi UTS

--- A2.1810004/Praktikum_2/siswa/form_tambah.php
+++ A2.1810004/Praktikum_2/siswa/form_tambah.php
@@ -57,8 +57,8 @@
 
 							<div class="form-group  col-md-12">
 								<label>Tanggal Lahir</label>
-								<input type="date" class="form-control  date-picker" data-date-format="dd-mm-yyyy" name="tanggal_lahir" autocomplete="off" required>
-								<div class="invalid-feedback">Tanggal Lahir tidak boleh kosong.</div>
+								<input  type="text"  class="form-control  date-picker"  data-date-format="dd-mm-yyyy" name="tanggal_lahir" autocomplete="off" required>
+								<div class="invalid-feedback">Tanggal Lahir tidak boleh kosong.</div> 	
 							</div>
 							<div class="form-group  col-md-12">
 								<label>Alamat</label>

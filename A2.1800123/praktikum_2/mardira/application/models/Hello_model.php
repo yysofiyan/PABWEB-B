6c6fb934d06193879c69102ee10d200d80847666
SintaM <A2.1800123@mhs.stmik-sumedang.ac.id>
2020-04-18 20:14:25 +0700

    Praktikum Codeigniter 1-7

---
+++ A2.1800123/praktikum_2/mardira/application/models/Hello_model.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Hello_model extends CI_Model
+{
+
+    public function hello_mardira()
+    {
+        return "Hello Mardira";
+    }
+
+    public function mardira_mvc()
+    {
+        return "Ini menggunakan MVC";
+    }
+}

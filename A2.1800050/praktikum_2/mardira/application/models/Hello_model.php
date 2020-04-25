759296161e4caccb18b6e9041c17babfbb3e67a2
uji312 <a2.1800050@mhs.stmik-sumedang.ac.id>
2020-04-25 23:15:36 +0700

    mengerjakan modul praktikum codeigniter

---
+++ A2.1800050/praktikum_2/mardira/application/models/Hello_model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+
+    public function hello_mardira()
+    {
+        return "Hello Mardira";
+    }
+    public function mardira_mvc()
+    {
+        return "Ini menggunakan MVC";
+    }
+}
---
+++ A2.1800050/praktikum_2/mardira/application/views/Halaman_utama_v.php
@@ -0,0 +1,8 @@
+<h1>Halaman Utama</h1>
+<ul>
+<li><?php echo anchor('mhs', 'Data Mahasiswa') ?></li>
+<li><?php echo anchor ('prodi', 'Data Program Studi') ?></li>
+</li>
+</ul>
+<?php echo anchor('logout', 'Logout') ?>
+</ul>

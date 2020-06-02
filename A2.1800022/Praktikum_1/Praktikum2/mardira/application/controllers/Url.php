047e113294cb636fbdd87dcbffc7aa2fff285152
ApipFebriansyah22 <a2.1800022@mhs.stmik-sumedang.ac.id>
2020-06-02 15:53:50 +0700

    praktikumCodeIgniter

---
+++ A2.1800022/Praktikum_1/Praktikum2/mardira/application/controllers/Url.php
@@ -0,0 +1,22 @@
+<?php
+
+class Url extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('url_view');
+    }
+
+    public function detail($id)
+    {
+        $id = $this->uri->segment(3);
+        $data['segment'] = $id = $id;
+        $this->load->view('halaman_detail', $data);
+    }
+
+    public function blank()
+    {
+        echo "halaman ini kosong silahkan kembali";
+        ('halaman');
+    }
+}

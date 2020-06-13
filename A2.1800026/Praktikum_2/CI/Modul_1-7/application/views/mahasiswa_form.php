e6f9003725f35ca36d8b6525a40008b1076677cc
Anurfah <a2.1800026@mhs.stmik-sumedang.ac.id>
2020-06-13 15:55:23 +0700

    menambahkan tugas CI modul 1-7

---
+++ A2.1800026/Praktikum_2/CI/Modul_1-7/application/views/mahasiswa_form.php
@@ -0,0 +1,21 @@
+<h3>Tambah Data</h3>
+
+<?php echo $form_open ?>
+<p>
+    <?php echo $label_nim ?>
+    <?php echo $input_nim ?>
+    <?php echo $error_nim ?>
+</p>
+<p>
+    <?php echo $label_nama ?>
+    <?php echo $input_nama ?>
+    <?php echo $error_nama ?>
+</p>
+<p>
+    <?php echo $label_prodi ?>
+    <?php echo $dropdown_prodi ?>
+</p>
+<p>
+    <?php echo $form_submit ?>
+</p>
+<?php echo form_close() ?>

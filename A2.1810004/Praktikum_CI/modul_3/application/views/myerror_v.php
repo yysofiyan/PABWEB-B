37729d086d377521f5be65981ddbba8c0546db7a
fadhliatul13 <a2.1810004@mhs.stmik-sumedang.ac.id>
2020-06-20 07:27:45 +0700

    hapus

--- A2.1810004/Praktikum_CI/modul_3/application/views/myerror_v.php
+++
@@ -1,1 +0,0 @@
-<h1>Ups, halaman tidak ditemukan!</h1>
--- A2.1810004/Praktikum_CI/modul_6/application/models/Auth_model.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Auth_model extends CI_model {
-    public function cek_user($data) {
-        return $this->db->get_where('users', $data);
-    }
-}

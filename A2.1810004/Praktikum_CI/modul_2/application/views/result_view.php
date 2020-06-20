af6183ac823e8f7b38aa91c906400f24bda6b735
fadhliatul13 <a2.1810004@mhs.stmik-sumedang.ac.id>
2020-06-20 07:33:23 +0700

    membuat Praktikum CI

---
+++ A2.1810004/Praktikum_CI/modul_2/application/views/result_view.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mahasiswa</title>
+</head>
+<body>
+    <table border="2">
+        <thead>
+            <tr>
+                <th>NO.</th>
+                <th>NIM</th>
+                <th>NAMA</th>
+            </tr>    
+        </thead>
+        <tbody>
+            <?php
+            $no = 1;
+            foreach ($result as $row) { ?>
+                <tr>
+                    <td><?php echo $no++ ?></td>
+                    <td><?php echo $row->NIM ?></td>
+                    <td><?php echo $row->nama_mhs ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
---
+++ A2.1810004/Praktikum_CI/modul_5/database/f_akbar .sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 18 Mei 2020 pada 07.42
+-- Versi Server: 5.6.21
+-- PHP Version: 5.5.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `f_akbar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
+`id_mahasiswa` int(3) NOT NULL,
+  `nim` int(10) NOT NULL,
+  `nama_mhs` varchar(30) NOT NULL,
+  `id_prodi` int(3) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id_mahasiswa`, `nim`, `nama_mhs`, `id_prodi`) VALUES
+(1, 18119123, 'Fadhliatul Akbar', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `prodi`
+--
+
+CREATE TABLE IF NOT EXISTS `prodi` (
+`id_prodi` int(3) NOT NULL,
+  `nama_prodi` varchar(30) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `prodi`
+--
+
+INSERT INTO `prodi` (`id_prodi`, `nama_prodi`) VALUES
+(1, ' Teknik Informatika '),
+(2, ' Manajemen Informatika ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+ ADD PRIMARY KEY (`id_mahasiswa`), ADD KEY `id_prodi` (`id_prodi`);
+
+--
+-- Indexes for table `prodi`
+--
+ALTER TABLE `prodi`
+ ADD PRIMARY KEY (`id_prodi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+MODIFY `id_mahasiswa` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `prodi`
+--
+ALTER TABLE `prodi`
+MODIFY `id_prodi` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
